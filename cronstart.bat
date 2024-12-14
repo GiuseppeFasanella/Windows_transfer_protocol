be6f8bffe2f36c5eca5b8dd31ba70bbf06dcb655
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2024-12-14 20:33:25 +0100

    Add files via upload

---
+++ cronstart.bat
@@ -0,0 +1,3 @@
+@REM This is a batch file, the equivalent for Windows of a bash file; REM are comments
+@REM cronstart.sh is here: peppe@CHSTL48VD6G3:~$ ls /usr/local/bin/ (in WSL partition obviously)
+C:\Windows\System32\wsl.exe sudo cronstart.sh
